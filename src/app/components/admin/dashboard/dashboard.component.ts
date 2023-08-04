cebd8bc8c16ed811f20a305e0a0c4cec0e0a33b1
durgesh tiwari <55891143+LearnCodeWithDurgesh@users.noreply.github.com>
2023-08-04 22:22:37 +0530

    updated-css

--- src/app/components/admin/dashboard/dashboard.component.ts
+++ src/app/components/admin/dashboard/dashboard.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
-  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
   adminMenus = [
